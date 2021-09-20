aedd9f1f325ab2e169cf910c1701f9cb62c73399
aaaasif <alexasif4@gmail.com>
2021-09-20 19:16:07 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name:'phone', price:'30000'},
+    {name:'camera', price:'53000'},
+    {name:'Laptop', price:'130000'},
+    {name:'Mac-Book', price:'230000'},
+    {name:'HearPhone', price:'6000'},
+    {name:'Watch', price:'45000'},
+  ]
+}
+
+function Product(props) {
+  const productStyle ={
+    border: '2px solid blue'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='product' style='productStyle'>
+      <h2>name: {props.name}</h2>
+      <h4>price: {props.price}</h4>
     </div>
-  );
+  )
 }
 
 export default App;
